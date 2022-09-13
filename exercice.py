e6b765d89b59b0f437ac55455781281311bf9689
Vincent Drolet <1437149@etu.cegep-lanaudiere.qc.ca>
2022-09-13 13:21:23 -0400

    Fonctions bills et average

--- exercice.py
+++ exercice.py
@@ -13,22 +13,39 @@ def orthogonal(v1, v2):
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	sum = 0
+	count = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		# La variable v contient une valeur de la liste.
+		if v >= 0:
+			sum += v
+			count += 1
+
+	if count > 0:
+		average = sum / count
+	else:
+		average = None
+	return average
+		
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = tens = fives = ones = 0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties += 1
+			value -= 20
 		elif value >= 10:
-			pass
+			tens += 1
+			value -= 10
 		elif value >= 5:
-			pass
+			fives += 1
+			value -= 5
 		elif value >= 1:
-			pass
+			ones += 1
+			value -= 1
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -44,8 +61,8 @@ def format_base(value, base, digit_letters):
 
 
 if __name__ == "__main__":
-	print(dissipated_power(69, 420))
-	print(orthogonal((1, 1), (-1, 1)))
+	#print(dissipated_power(69, 420))
+	#print(orthogonal((1, 1), (-1, 1)))
 	print(average([1, 4, -2, 10]))
 	print(bills(137))
-	print(format_base(-42, 16, "0123456789ABCDEF"))
+	#print(format_base(-42, 16, "0123456789ABCDEF"))
